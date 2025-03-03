fadb8dcd74811f6a0211df836693888d3953f9a4
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-03-03 14:50:42 +0100

    feat: complete 3 snack filtra iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,36 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+// function findLetterName(names, letter) {
+//     const namesWithLetter = [];
+//     const capitalLetter = letter.toUpperCase();
+
+//     for (let i = 0; i < names.length; i++) {
+//         let currentName = names[i];
+//         if (currentName[0] === capitalLetter) {
+//             namesWithLetter.push(names[i]);
+//         }
+//     }
+//     return namesWithLetter;
+// }
+
+const findLetterName = (names, letter) => {
+    const namesWithLetter = [];
+    const capitalLetter = letter.toUpperCase();
+
+    for (let i = 0; i < names.length; i++) {
+        let currentName = names[i];
+        if (currentName[0] === capitalLetter) {
+            namesWithLetter.push(names[i]);
+        }
+    }
+    return namesWithLetter;
+};
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(findLetterName(names, 'A'));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
